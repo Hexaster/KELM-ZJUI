d17d9e047cf72e4932279a3c7f3731c99075489d
Yihang Zhu <u7630167@anu.edu.au>
2025-03-06 11:46:16 +0800

    Rudimentary Web Interface

--- Web/chat/views.py
+++ Web/chat/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def chat_view(request):
+    messages = request.session.get('chat_messages', [])
+    if request.method == 'POST':
+        user_message = request.POST.get('message')
+        if user_message:
+            messages.append({'text': user_message, 'user': True})
+            request.session['chat_messages'] = messages
+
+    return render(request, 'chat/chat.html', {'messages': messages})
